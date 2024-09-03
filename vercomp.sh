18371d837c5c57b08a760e7370fc25927fdd350c
Jiri Sztuka <jsztuka@redhat.com>
2024-09-03 13:41:11 +0200

    getlate

--- vercomp.sh
+++ vercomp.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 ver1="1.0.6"
-ver2="ClamAV 1.0.6.1"
+ver2="ClamAV 1.0.7"
 
 ver1=$(echo $ver1 | sed 's/[^0-9]*//g')
 ver2=$(echo $ver2 | sed 's/[^0-9]*//g')
 
 if [ $ver1 -lt $ver2 ]; then
     echo "yes, higher"
+    ((ver2++))
 else
     echo "naah"
 fi
+
+echo "$ver2"
